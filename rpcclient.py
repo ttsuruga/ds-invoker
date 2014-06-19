2b805456ec2d3b84c9ec64d6aeb250837b89a3b0
Takenori Tsuruga <ttsuruga@csusb.edu>
2014-06-18 18:21:50 -0700

    clean up

--- rpcclient.py
+++ rpcclient.py
@@ -6,11 +6,9 @@ master = Tk()
 s = xmlrpc.client.ServerProxy('http://localhost:8000')
 
 def openlink():
-  #s.browse("http://news.google.com")
-  s.browse("file:///C:/Users/take2/Desktop/pytest-2014-06-11/pytest/canvas.html")
+  s.browse("http://news.google.com")
 
 b = Button(master, text="OK", command=openlink)
 b.pack()
 
 mainloop();
-
